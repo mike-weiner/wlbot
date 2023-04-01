6fe51290c559b2728ea8aa438cf6250f1e182f11
Michael Weiner <vikings412@gmail.com>
2023-04-01 13:25:58 -0500

    fix bug when env. vars. are missing

--- commands/config.js
+++ commands/config.js
@@ -5,14 +5,14 @@ export default (options) => {
   const spinner = !options.raw ? ora('Reading Environment Variables').start() : undefined;
 
   let evValues = {
-    "WEATHER_LINK_API_KEY": process.env.WEATHER_LINK_API_KEY,
-    "WEATHER_LINK_API_SECRET": process.env.WEATHER_LINK_API_SECRET,
-    "WEATHER_LINK_BASE_API_URL": process.env.WEATHER_LINK_BASE_API_URL,
+    "WEATHER_LINK_API_KEY": process.env.WEATHER_LINK_API_KEY || "",
+    "WEATHER_LINK_API_SECRET": process.env.WEATHER_LINK_API_SECRET || "",
+    "WEATHER_LINK_BASE_API_URL": process.env.WEATHER_LINK_BASE_API_URL || "",
   }
 
   if (!options.list) {
     for (const [key, value] of Object.entries(evValues)) {
-      evValues[key] = evValues[key].slice(0, 3) + "*".repeat(value.length - evValues[key].slice(0, 3).length)
+      evValues[key] = evValues[key].slice(0, 3) + "*".repeat(value.length - evValues[key].slice(0, 3).length);
     }
   }
 
