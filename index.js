02978923ee30b8918f6be0962bbb60b386778d90
Michael Weiner <vikings412@gmail.com>
2023-04-01 13:26:42 -0500

    bump version to 1.1.1

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const program = new Command();
 program
   .description("A CLI for the WeatherLink Live API.")
   .name("wlbot")
-  .version('1.1.0')
+  .version('1.1.1')
   .usage('<command>');
 
 const metadata = program.command("metadata")
