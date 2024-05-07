41190e66f99c6be67fd360407f0db0d440711cc8
Michael Weiner <47571709+mike-weiner@users.noreply.github.com>
2024-05-07 18:35:37 -0500

    Prep Release of `v1.1.8` (#46)
    * bump CLI version to 1.1.8
    
    * 1.1.8

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const program = new Command();
 program
   .description("A CLI for the WeatherLink Live API.")
   .name("wlbot")
-  .version('1.1.7')
+  .version('1.1.8')
   .usage('<command>');
 
 const metadata = program.command("metadata")
