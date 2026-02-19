e2bfb103d9ec4ee84bcadcde06a4dda9d3930dce
Michael Weiner <vikings412@gmail.com>
2026-02-19 11:57:02 -0600

    Install vitest

---
+++ vitest.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      thresholds: {
+        branches: 95,
+        functions: 95,
+        lines: 95,
+        statements: -30,
+      },
+      include: ['**/*.{js,jsx}'],
+      exclude: [
+        '**/node_modules/**',
+        '**/coverage/**',
+        'vitest.config.js',
+        'index.js'
+      ],
+    },
+    setupFiles: ['./.vitest/setEnvVars.js'],
+    globals: false
+  },
+});
